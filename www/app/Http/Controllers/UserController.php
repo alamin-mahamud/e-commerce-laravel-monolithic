69a1840a4e09d1c5b9a70cd4336ef5864a1ddc01
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-02-14 22:42:21 +0600

    :sparkles: User Controller

--- www/app/Http/Controllers/UserController.php
+++ www/app/Http/Controllers/UserController.php
@@ -11,4 +11,46 @@ class UserController extends Controller
     {
         return response()->json(User::with(['orders']))->get();
     }
+
+    public function login(Request $request)
+    {
+        $status = 401;
+        $response = [
+            'error' => 'Unauthorised'
+        ];
+
+        if (Auth::attempt($request->only(['email', 'password']))) {
+            $status = 200;
+            $response = [
+                'user' => Auth::user(),
+                'token' => Auth::user()->createToken('bigStore')->accessToken,
+            ];
+        }
+
+        return response()->json($response, $status);
+    }
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|max:50',
+            'email' => 'required|email',
+            'password' => 'required|min:6',
+            'c_password' => 'required|same:password',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+    }
+
+    public function show(User $user)
+    {
+        return response()->json($user);
+    }
+
+    public function showOrders(User $user)
+    {
+        return response()->json($user->orders()->with(['product'])->get());
+    }
 }
