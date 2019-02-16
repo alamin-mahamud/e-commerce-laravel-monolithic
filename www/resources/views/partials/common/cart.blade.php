ed6397ac63190a4027591473ef539192e594dee5
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-02-16 23:42:38 +0600

    :fire: delete partials

--- www/resources/views/partials/common/cart.blade.php
+++
@@ -1,9 +0,0 @@
-@php
-    $cartItemsCount = Cart::instance('default')->count();
-@endphp
-
-@if ( $cartItemsCount > 0)
-    <span class="cart-count">
-        <span>{{$cartItemsCount}}</span>
-    </span>
-@endif
