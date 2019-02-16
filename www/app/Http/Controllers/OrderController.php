4c89cfd5eecf1feb580f1158c6d1a4adf5345fd2
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-02-16 13:29:16 +0600

    :sparkles: $MSG

--- www/app/Http/Controllers/OrderController.php
+++ www/app/Http/Controllers/OrderController.php
@@ -14,7 +14,7 @@ class OrderController extends Controller
      */
     public function index()
     {
-        //
+        return response()->json(Order::with(['product'])->get(), 200);
     }
 
     /**
@@ -35,7 +35,18 @@ class OrderController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $order = Order::create([
+            'product_id' => $request->product_id,
+            'user_id' => Auth::id(),
+            'quantity' => $request->quantity,
+            'address' => $request->address
+        ]);
+
+        return response()->json([
+            'status' => (bool)$order,
+            'data' => $order,
+            'message' => $order ? 'Order Created!' : 'Error Creating Order'
+        ]);
     }
 
     /**
@@ -46,7 +57,7 @@ class OrderController extends Controller
      */
     public function show(Order $order)
     {
-        //
+        return response()->json($order, 200);
     }
 
     /**
@@ -69,7 +80,14 @@ class OrderController extends Controller
      */
     public function update(Request $request, Order $order)
     {
-        //
+        $status = $order->update(
+            $request->only(['quanttity'])
+        );
+
+        return response()->json([
+            'status' => $status,
+            'message' => $status ? 'Order Updated!' : 'Error Updating Order'
+        ]);
     }
 
     /**
@@ -80,6 +98,23 @@ class OrderController extends Controller
      */
     public function destroy(Order $order)
     {
-        //
+        $status = $order->delete();
+
+        return response()->json([
+            'status' => $status,
+            'message' => $status ? 'Order Deleted!' : 'Error Deleting Order'
+        ]);
+    }
+
+    public function deliverOrder(Order $order)
+    {
+        $order->is_delivered = true;
+        $status = $order->save();
+
+        return response()->json([
+            'status' => $status,
+            'data' => $order,
+            'message' => $status ? 'Order Delivered!' : 'Error Delivering Order'
+        ]);
     }
 }
