1fa8f8fa18c9eac02b4653c285143ff0e34da1da
Md. Alamin Mahamud <alamin.ineedahelp@gmail.com>
2019-02-16 23:40:32 +0600

    :sparkles: landing page changed according to vue js

--- www/resources/views/landingPage/partials/body.blade.php
+++ www/resources/views/landingPage/partials/body.blade.php
@@ -1,4 +1,10 @@
 <body>
+
     <div id="app">
+        <app></app>
     </div>
+
+    <script src="{{mix('js/bootstrap.js')}}"></script>
+    <script src="{{mix('js/app.js')}}"></script>
+
 </body>
--- www/resources/views/landingPage/partials/head.blade.php
+++ www/resources/views/landingPage/partials/head.blade.php
@@ -1,7 +1,9 @@
+<!DOCTYPE html>
 <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{csrf_token()}}">
 
         <title>Laravel Ecommerce Example</title>
 
@@ -10,7 +12,5 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/responsive.css') }}">
-
+        <link rel="stylesheet" href="{{mix('css/app.css')}}">
 </head>
